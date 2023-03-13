96c2dd67beacf54ae62aa21b6fdf0a81cbd1a3f6
John Hall <johnhall2.0@gmail.com>
2023-03-13 05:44:49 -0400

    Added alot of code for managing the table views for peers and listener ports.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,89 @@ use cursive::theme;
 use cursive::traits::*;
 use cursive_table_view::{TableView,TableViewItem};
 
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+enum PeerCol {Id, Role, maddr, live}
+
+impl PeerCol {
+    fn as_str(&self) -> &str{
+        match *self{
+            PeerCol::id => "ID",
+            PeerCol::role => "Role",
+            PeerCol::maddr => "Maddr",
+            PeerCol::live => "live",
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq,Hash)]
+enum PeerRole{ Dialer, Listener, Relay}
+impl PeerRole {
+    fn as_str(&self) -> &str{
+        match *self{
+            PeerRole::Dialer => "D",
+            PeerRole::Listenr => "L",
+            PeerRole::Relay => "R",
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct PeerRow {
+    id: String,
+    role: PeerRole,
+    maddr: String,
+    live: bool,
+}
+
+Impl TableViewItem<PeerCol> for PeerRow {
+    fn to_column(&self, column: PeerCol) -> String {
+        match column{
+            PeerCol::id => self.id.to_string(),
+            PeerCol::maddr => self.maddr.to_string(),
+            PeerCol::role => self.role.to_string(),
+            PeerCol::live => if self.live {"*"} else {" "},
+        }
+    }
+}
+
+
+
+
+#[derive(Copy, Clone, Hash, Debug)]
+struct InstanceInfo{
+    id: String,
+    host: String,
+    working_directory: String,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+
+enum ListenerCol{
+    maddr, connection_count
+}
+impl ListenerCol {
+    fn as_str(&self) -> &str{
+        match *self{
+            ListenerCols::maddr => "Maddr",
+            ListenerCols::connection_count => "ConCount",
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+struct ListenerRow{
+    maddr:String,
+    connection_count: u16,
+}
+Impl TableViewItem<ListenerCol> for ListenerRows {
+    fn to_column(&self, column: ListenerCol) -> String {
+        match column{
+            ListenerCol::maddr => self.maddr.to_string(),
+            ListenerCol::connection_count => format!("{}",self.connection_count),
+        }
+    }
+}
+
 fn main() {
     let args = Arguments::parse();
     println!("{:?}",args);
@@ -29,7 +112,8 @@ fn main() {
         .on_submit(on_submit)
         .with_name("select")
         .fixed_size((10, 5));
-    
+    let peers_and_ports= LinearLayout::horizontal().
+        .child(
     let buttons = LinearLayout::vertical()
         .child(Button::new("Add new", add_name))
         .child(Button::new("Delete", delete_name))
